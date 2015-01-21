6f13b299d23c72a1be90b9fd901b3742d12f36c5
Frank Yao <yaofangjie2010@126.com>
2015-01-21 11:24:04 +0800

    some dynamic effect modified

--- xichao/static/js/main.js
+++ xichao/static/js/main.js
@@ -20,29 +20,36 @@ $(document).ready(function(){
     /**************************************************************/
 
     var login_circle_onMouseIn = function(e){      
-        $(this).stop(true, true);
 
         var mousePosX = e.pageX;
         var mousePosY = e.pageY;
+
         var circlePosX = 643;
-        var circlePosY = -7;
+        var circlePosY = -7; //获取对象位置，绝对坐标，防止圆离开最初位置
 
-        var timeAway = 100, timeBack = 650;
-        var distance = 15;
+        var circleDiameter = parseInt($("#login-inside-circle").css("height").slice(0, -2)); //圆的直径
+        var circleLeftBorder = parseInt($("#login-inside-circle").offset().left);
+        var circleTopBorder = parseInt($("#login-inside-circle").offset().top); //圆的位置
 
-        //确定鼠标位置
-        if(mousePosX < 960)
+        var timeAway = 100, timeBack = 650; //拨过去和回来的时间(ms)
+        var distance = 15; //拨的幅度(px)
+
+        $(this).stop(true, true);
+
+        //确定鼠标位置，完成jQuery动画
+        if(mousePosX < circleLeftBorder + 0.25*circleDiameter)
             $(this).animate({left: circlePosX+distance+"px"}, timeAway)
                .animate({left: circlePosX+"px", top: circlePosY+"px"}, timeBack);
-        else if(mousePosX > 1180)
+        else if(mousePosX > circleLeftBorder + 0.75*circleDiameter)
             $(this).animate({left: circlePosX-distance+"px"}, timeAway)
                .animate({left: circlePosX+"px", top: circlePosY+"px"}, timeBack);
-        else if(mousePosY < 430)
+        else if(mousePosY < circleTopBorder + 0.25*circleDiameter)
             $(this).animate({top: circlePosY+distance+"px"}, timeAway)
                .animate({top: circlePosY+"px", left: circlePosX+"px"}, timeBack);
-        else if(mousePosY > 650)
+        else if(mousePosY > circleTopBorder + 0.75*circleDiameter)
             $(this).animate({top: circlePosY-distance+"px"}, timeAway)
                .animate({top: circlePosY+"px", left: circlePosX+"px"}, timeBack);
+
     };
 
     var login_circle_onMouseOut = function(){
@@ -57,7 +64,6 @@ $(document).ready(function(){
     /**************************************************************/
     $(".login-userinfo > #email").attr({"placeholder": "输入用户名/邮箱"});
     $(".login-userinfo > #password").attr({"placeholder": "密码"});
-
 
 
 
