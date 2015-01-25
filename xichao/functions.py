da83a937d694a1dc52add6e4ec81b7c689273dc6
Frank Yao <yaofangjie2010@126.com>
2015-01-25 19:24:56 +0800

    忘记密码模块完成

--- xichao/functions.py
+++ xichao/functions.py
@@ -76,8 +76,19 @@ def get_nick(email,password):
 		return False
 
 ##################################  忘记/重设密码 #################################
+#通过email获取nick
+def get_nick_by_email(email):
+	nick = db_session.query(User.nick).filter_by(email=email).first()
+	return nick[0]
+
+#通过email获取password
+def get_password_by_email(email):
+	password = db_session.query(User.password).filter_by(email=email).first()
+	return password[0]
+
+#发送重设密码邮件
 def send_resetpassword_email(nick,password,email):
-	verify_url=app.config['HOST_NAME']+'/resetPassword/'+nick+'/'+encrypt(password) #/nick/MD5(password)
+	verify_url=app.config['HOST_NAME']+'/resetPassword/'+nick+'/'+password #/nick/MD5(password)
 	mail=Mail(app)
 	msg=Message(u'重置曦潮网站的密码',sender='xichao_test@163.com',recipients=[email])
 	msg.body='text body'
@@ -85,8 +96,21 @@ def send_resetpassword_email(nick,password,email):
 	with app.app_context():
 		mail.send(msg)
 
+#是否存在该用户名，用户名和密码是否匹配
 def check_nickpassword_match(nick, password):
-	return True
+	pw = db_session.query(User.password).filter_by(nick=nick).all()
+	if not len(pw): #不存在nick
+		return False 
+	elif pw[0][0] != password:
+		return False
+	else:
+		return True
+
+#更新密码
+def update_password(nick, password):
+	user = db_session.query(User).filter_by(nick=nick).scalar()
+	user.password = encrypt(password)
+	db_session.commit()
 
 ##################################  文章函数  ####################################
 def get_article_session_id():
