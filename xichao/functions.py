cfcc242fea36b2a1826ed33884b1a15671b3db40
Frank Yao <yaofangjie2010@126.com>
2015-02-03 19:46:46 +0800

    别人看的文章页完成

--- xichao/functions.py
+++ xichao/functions.py
@@ -6,7 +6,7 @@
 	实现一些数据库基本的功能，被高层次代码所调用
 
 '''
-#from xichao import app
+from xichao import app
 from hashlib import md5
 from models import User,Article,Special,Book,Comment,Article_session,Activity_session,Activity,Comment_activity,Collection_Special,Collection_User,Collection_Article,Collection_Activity
 from database import db_session
@@ -19,7 +19,6 @@ from flask.ext.mail import Message
 from flask.ext.sqlalchemy import Pagination
 import re
 import models
-#from sqlalchemy.orm import query
 
 
 ##################################  注册函数  ####################################
