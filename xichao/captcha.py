d8f43b013ccf2649b0cb5285955ce1f49eaea467
朱亮 <torres@zhuliangdeMacBook-Air.local>
2015-04-13 19:22:23 +0800

    test

--- xichao/captcha.py
+++ xichao/captcha.py
@@ -1,7 +1,7 @@
 #coding=utf-8 
 from flask import Flask, send_file, url_for
 import random
-import Image, ImageDraw, ImageFont, ImageFilter 
+from PIL import Image, ImageDraw, ImageFont, ImageFilter 
 import StringIO
 import urllib
 import os
