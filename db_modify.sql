dc92e50dd3b0e4928454c66c39c2418469525e7b
lxserenade <lixia625@hotmail.com>
2015-04-18 14:19:43 +0800

    nested comment, to be rewrote

--- db_modify.sql
+++ db_modify.sql
@@ -1,10 +1,5 @@
 alter  table comment add root int(11);
-
 alter table comment add constraint root foreign key(root) REFERENCES user(user_id);
-
-
 alter  table comment add father int(11);
 alter table comment add constraint father foreign key(father) REFERENCES user(user_id);
-
-
 alter table comment modify column content text;
