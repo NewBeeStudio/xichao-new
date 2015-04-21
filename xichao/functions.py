0289b837ee53620cde6eed2073def1cb3c57db02
lxserenade <lixia625@hotmail.com>
2015-04-21 22:04:01 +0800

    add http

--- xichao/functions.py
+++ xichao/functions.py
@@ -69,7 +69,7 @@ def get_secure_photoname(filename):
 	return photoname
 
 def send_verify_email(nick,password,email):
-    verify_url=app.config['HOST_NAME']+'/verify?nick='+nick+'&secret='+password
+    verify_url='http://'+app.config['HOST_NAME']+'/verify?nick='+nick+'&secret='+password
     mail=Mail(app)
     msg=Message(u'曦潮书店',sender='xichao_test@163.com',recipients=[email])
     msg.body='text body'
@@ -102,7 +102,7 @@ def get_password_by_email(email):
 
 #发送重设密码邮件
 def send_resetpassword_email(nick,password,email):
-	verify_url=app.config['HOST_NAME']+'/resetPassword/'+nick+'/'+password #/nick/MD5(password)
+	verify_url='http://'+app.config['HOST_NAME']+'/resetPassword/'+nick+'/'+password #/nick/MD5(password)
 	mail=Mail(app)
 	msg=Message(u'重置曦潮网站的密码',sender='xichao_test@163.com',recipients=[email])
 	msg.body='text body'
