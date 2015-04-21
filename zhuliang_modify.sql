193049d95daa46e50ae540200a62a315d83e3a67
yhzhang0128 <zhangyunh@gmail.com>
2015-04-20 19:20:47 -0700

    修改朱亮的sql文件，添加use xichao;

--- zhuliang_modify.sql
+++ zhuliang_modify.sql
@@ -1,3 +1,4 @@
+use xichao;
 alter table book modify column author varchar(255);
 alter table homepage add ground_recommended_article int(11) not null default 1;
 alter table homepage add constraint FK_ID foreign key(ground_recommended_article) references article(article_id);
