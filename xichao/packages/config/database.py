8ede6fc98822d52639e3e6e78eed7929fe292485
yhzhang0128 <zhangyunh@gmail.com>
2015-08-21 19:08:07 -0700

    Modify html,css,js by YaoYuan

--- xichao/packages/config/database.py
+++ xichao/packages/config/database.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 #本地
-#_RDS_DB_URL_ = 'mysql://root:Xichao42@localhost/xichao?charset=utf8'
+_RDS_DB_URL_ = 'mysql://root:Xichao42@localhost/xichao?charset=utf8'
 #RDS服务器
-_RDS_DB_URL_ ='mysql://xichao:Xichao42@rdszfuv6jmjjbei.mysql.rds.aliyuncs.com:3306/xichao?charset=utf8'
+#_RDS_DB_URL_ ='mysql://xichao:Xichao42@rdszfuv6jmjjbei.mysql.rds.aliyuncs.com:3306/xichao?charset=utf8'
